e8e61f6a97e0ecd3b7b249ce2319cd33b5cabd0e
thereallda <lida@sioc.ac.cn>
2022-03-02 21:53:14 +0800

    ch13-note

--- Ch13/pca.py
+++ Ch13/pca.py
@@ -8,7 +8,7 @@ from numpy import *
 def loadDataSet(fileName, delim='\t'):
     fr = open(fileName)
     stringArr = [line.strip().split(delim) for line in fr.readlines()]
-    datArr = [map(float,line) for line in stringArr]
+    datArr = [list(map(float,line)) for line in stringArr]
     return mat(datArr)
 
 def pca(dataMat, topNfeat=9999999):
@@ -19,7 +19,7 @@ def pca(dataMat, topNfeat=9999999):
     eigValInd = argsort(eigVals)            #sort, sort goes smallest to largest
     eigValInd = eigValInd[:-(topNfeat+1):-1]  #cut off unwanted dimensions
     redEigVects = eigVects[:,eigValInd]       #reorganize eig vects largest to smallest
-    lowDDataMat = meanRemoved * redEigVects#transform data into new dimensions
+    lowDDataMat = meanRemoved * redEigVects #transform data into new dimensions
     reconMat = (lowDDataMat * redEigVects.T) + meanVals
     return lowDDataMat, reconMat
 
